594783d98ac2abcd11c3fcecd3bc40971f6fdfd3
Oleg Stirbu <rikeroleg@gmail.com>
2024-07-31 13:30:45 -0400

    db

--- src/main/java/com/masterchefcuts/model/Participant.java
+++ src/main/java/com/masterchefcuts/model/Participant.java
@@ -1,69 +1,31 @@
 package com.masterchefcuts.model;
 
+import org.hibernate.annotations.UuidGenerator;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "participants")
 public class Participant {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
-
-    public Participant() {
-    }
-
-    public Participant(int id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Participant{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+    private String phone;
+    private String address;
+    private String status;
 }
--- src/main/java/com/masterchefcuts/services/ParticipantServiceImpl.java
+++ src/main/java/com/masterchefcuts/services/ParticipantServiceImpl.java
@@ -2,11 +2,13 @@ package com.masterchefcuts.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.masterchefcuts.model.Participant;
 import com.masterchefcuts.repositories.ParticipantRepo;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class ParticipantServiceImpl implements ParticipantService {
 
     @Autowired
@@ -16,5 +18,14 @@ public class ParticipantServiceImpl implements ParticipantService {
     public Participant addParticipant(Participant participant) {
         return participantRepo.save(participant);
     }
-    
+
+    @Override
+    public Participant findById(String id) {
+        return participantRepo.findById(id).orElseThrow(() -> new RuntimeException("Participant not Found")); 
+    }
+
+    // @Override
+    // public void deleteParticipant(String id) {
+    //     return participantRepo.delete(id);
+    // }
 }
